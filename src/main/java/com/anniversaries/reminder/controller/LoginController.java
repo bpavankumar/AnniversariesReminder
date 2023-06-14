455df40f70022b9b1db7e6eff7a38c5bb3e40003
B Pavan Kumar <pavan.b9@gmail.com>
2023-06-14 11:02:20 +0530

    Introducing initial flow

--- src/main/java/com/anniversaries/reminder/controller/LoginController.java
+++ src/main/java/com/anniversaries/reminder/controller/LoginController.java
@@ -1,10 +1,13 @@
 package com.anniversaries.reminder.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class LoginController {
-	@GetMapping("/")
+
+	@GetMapping("/login")
 	public String login(Model model) {
 		return "login";
 	}
