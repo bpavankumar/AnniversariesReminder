428220c793843a1a00b8dbc6a6a1d982ffbd059c
B Pavan Kumar <pavan.b9@gmail.com>
2023-06-16 13:33:19 +0530

    Adding SpringSecurity flow, but still not working though

---
+++ src/main/java/com/anniversaries/reminder/security/AnniversariesSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.anniversaries.reminder.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class AnniversariesSecurityConfig extends WebSecurityConfiguration {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder authBuilder) throws Exception {
+		authBuilder.jdbcAuthentication()
+			.dataSource(dataSource)
+			.passwordEncoder(new BCryptPasswordEncoder())
+			.usersByUsernameQuery("select username, password, enabled from users where username=?")
+			.authoritiesByUsernameQuery("select username, role from users where username=?");
+	}
+
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.requestMatchers("/edit/*", "/delete/*").hasRole("ADMIN")
+			.anyRequest().authenticated()
+			.and()
+			.formLogin().permitAll()
+			.and()
+			.logout().permitAll()
+			.and()
+			.exceptionHandling().accessDeniedPage("/403");
+	}
+}
