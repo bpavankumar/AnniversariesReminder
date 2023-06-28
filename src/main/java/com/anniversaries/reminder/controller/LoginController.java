36c87abb1086f99a3d391513f6f9a97ff9bf7089
B Pavan Kumar <pavan.b9@gmail.com>
2023-06-28 15:11:16 +0530

    Fix: Login credentials are no longer shown in the browser address bar

--- src/main/java/com/anniversaries/reminder/controller/LoginController.java
+++ src/main/java/com/anniversaries/reminder/controller/LoginController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.anniversaries.reminder.service.UsersService;
@@ -19,7 +21,7 @@ public class LoginController {
 		return "login";
 	}
 
-	@GetMapping("/home")
+	@RequestMapping(value="/home", method = RequestMethod.POST)
 	public String checkUserLogin(@RequestParam("user_id") String user_id, @RequestParam("password") String password, @RequestParam("role") String role) {
 		boolean enabled = role.equalsIgnoreCase("ADMIN") ? true : false;
 		if (usersService.checkUserLogin(user_id, password, enabled))
