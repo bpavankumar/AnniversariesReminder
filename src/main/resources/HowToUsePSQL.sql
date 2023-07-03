b0f3a42199b519024a3a26569171e3a04382df3f
B Pavan Kumar <pavan.b9@gmail.com>
2023-07-03 14:32:09 +0530

    PSQL self-documentation for future reference

---
+++ src/main/resources/HowToUsePSQL.sql
@@ -0,0 +1,16 @@
+--Create database command
+CREATE DATABASE reminders;
+
+--List the available databases
+\l
+
+--Connect to a specific database
+\c reminders
+
+--List the available tables on the connected database
+\d
+
+--Describe a particular table from a given database
+\d users
+
+INSERT INTO users VALUES()
