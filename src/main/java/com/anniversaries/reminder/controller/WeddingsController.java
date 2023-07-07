8694015289af63ab33cafb7beb23bea2f7958362
B Pavan Kumar <pavan.b9@gmail.com>
2023-07-07 16:28:51 +0530

    Implemented the weddings flow

--- src/main/java/com/anniversaries/reminder/controller/WeddingsController.java
+++ src/main/java/com/anniversaries/reminder/controller/WeddingsController.java
@@ -1,8 +1,13 @@
 package com.anniversaries.reminder.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.anniversaries.reminder.entity.WeddingAnniversaries;
 import com.anniversaries.reminder.service.WeddingsService;
 
 @Controller
@@ -10,4 +15,11 @@ public class WeddingsController {
 
 	@Autowired
 	public WeddingsService weddingsService;
+
+	@GetMapping("/admin/weddings")
+	public String allWeddings(Model model) {
+		List<WeddingAnniversaries> allWeddings = weddingsService.getAllWeddingAnniversaries();
+		model.addAttribute("weddings", allWeddings);
+		return "weddings";
+	}
 }
