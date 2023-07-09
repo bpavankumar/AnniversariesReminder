d89ed3992de891429fb78773c3af9bcaee579a0f
B Pavan Kumar <pavan.b9@gmail.com>
2023-07-09 21:29:59 +0530

    Removed the hardcoded values and invoked the respective Services for real date

--- src/main/java/com/anniversaries/reminder/service/OverviewService.java
+++ src/main/java/com/anniversaries/reminder/service/OverviewService.java
@@ -13,12 +13,14 @@ public class OverviewService {
 	@Autowired
 	WeddingsService weddingsService;
 	@Autowired
+	WorksService worksService;
+	@Autowired
 	DeathsService deathsService;
 
 	public Map<String, Integer> dataExtract() {
 		Map<String, Integer> data = new HashMap<>();
 		data.put("Birthdays", birthdaysService.getAllBirthdays().size());
-		data.put("Work Anniversaries", 0);
+		data.put("Work Anniversaries", worksService.getAllWorkAnniversaries().size());
 		data.put("Wedding Anniversaries", weddingsService.getAllWeddingAnniversaries().size());
 		data.put("Death Anniversaries", deathsService.getAllDeathAnniversaries().size());
 		return data;
