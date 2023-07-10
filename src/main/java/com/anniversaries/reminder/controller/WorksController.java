4105fd414551a8e3f820a2c8faad1e1fcbe67d44
B Pavan Kumar <pavan.b9@gmail.com>
2023-07-10 10:32:10 +0530

    Implemented the working flow of Work-anniversaries

--- src/main/java/com/anniversaries/reminder/controller/WorksController.java
+++ src/main/java/com/anniversaries/reminder/controller/WorksController.java
@@ -1,8 +1,13 @@
 package com.anniversaries.reminder.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.anniversaries.reminder.entity.WorkAnniversaries;
 import com.anniversaries.reminder.service.WorksService;
 
 @Controller
@@ -10,4 +15,11 @@ public class WorksController {
 
 	@Autowired
 	public WorksService worksService;
+
+	@GetMapping("/admin/works")
+	public String allWorkAnniversaries(Model model) {
+		List<WorkAnniversaries> allWorks = worksService.getAllWorkAnniversaries();
+		model.addAttribute("works", allWorks);
+		return "works";
+	}
 }
