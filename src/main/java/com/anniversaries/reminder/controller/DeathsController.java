b7c3c41b3e8f9cf2b060670eaca15d48fc9af69d
B Pavan Kumar <pavan.b9@gmail.com>
2023-07-22 13:04:36 +0530

    Resolved UnusedImports PMD

--- src/main/java/com/anniversaries/reminder/controller/DeathsController.java
+++ src/main/java/com/anniversaries/reminder/controller/DeathsController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.anniversaries.reminder.entity.Birthdays;
 import com.anniversaries.reminder.entity.DeathAnniversaries;
 import com.anniversaries.reminder.service.DeathsService;
 
